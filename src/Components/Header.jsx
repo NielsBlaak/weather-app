c4685291a8fd3c572b90af1ac8555ad898d00a9d
NielsBlaak <nielsblaak@hotmail.com>
2021-09-16 17:37:02 +0200

    implement tailwind

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import icon from '../assets/rotterdam.svg';
-// import menu from '../assets/menu.svg';
+import menu from '../assets/menu.svg';
 
 const Header = () => {
     return (
-        <div className="header">
+        <div className="w-full h-12 flex justify-between bg-primary">
             <a href="/">
-                <img src={icon} alt="icon" />
+                <img src={icon} alt="icon" className="h-full px-4" />
             </a>
-            <p>Rotterdams weer</p>
-            {/* <img src={menu} alt="icon" /> */}
+            <p className="text-center self-center font-bold text-2xl">Rotterdams weer</p>
+            <img src={menu} alt="icon" />
         </div>
     )
 }
